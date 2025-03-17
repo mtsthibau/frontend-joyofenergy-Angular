358eeb6867e1c894365c36b963031ad5accd9781
Matheus Thibau <mtsthibau@gmail.com>
2025-03-17 15:25:39 -0300

    Refactoring main component

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Data } from 'src/app/shared/models/dataModel';
-import { ApiService } from 'src/app/shared/services/api.service';
-import { renderChart } from "../../shared/utils/chart";
 import { groupByDay, sortByTime, getReadings } from "../../shared/utils/reading";
 
 @Component({
@@ -9,21 +7,22 @@ import { groupByDay, sortByTime, getReadings } from "../../shared/utils/reading"
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
+
+
 export class MainComponent implements OnInit {
-  chartData: Data[] = [];
-  constructor() { 
+
+  chartData: Data[] = []
+
+  constructor() {
     this.createChart();
   }
 
   ngOnInit(): void {
-     
   }
 
   async createChart() {
-    const readings = await getReadings();
-    const containerId = "chart";
-    this.chartData = readings;
-    renderChart(containerId, sortByTime(groupByDay(readings)).slice(-30));
+    this.chartData = await getReadings();
+    this.chartData = groupByDay(this.chartData)
+    this.chartData = sortByTime(this.chartData).slice(-30)
   }
-
 }
