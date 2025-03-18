801434fbdb2e8736d3935ab9b9a487776c77a11f
Matheus Thibau <mtsthibau@gmail.com>
2025-03-18 11:02:19 -0300

    Fixing tests

--- src/app/shared/services/api.service.spec.ts
+++ src/app/shared/services/api.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
@@ -12,5 +11,32 @@ describe('ApiService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return an array of readings', async () => {
+    const readings = await service.getReadings(10);
+    expect(readings.length).not.toBe(0);
+  })
+
+  it("should generate readings with specified length", async () => {
+    const length = 100;
+    expect(await service.getReadings(length)).toHaveSize(length);
+  });
+
+  it("should generate readings with timestamps and random values", async () => {
+    const reading = (await service.getReadings(1))[0];
+
+    expect(typeof reading.time).toBe("number");
+    expect(typeof reading.value).toBe("number");
+  });
+
+  it("should generate readings by hours and ordered by time descending", async () => {
+    const readings = await service.getReadings(4);
+
+    expect(readings).toHaveSize(4);
+    const OneHourInMilliseconds = 60 * 60 * 1000;
+    expect(readings[0].time - readings[1].time).toBe(OneHourInMilliseconds);
+    expect(readings[1].time - readings[2].time).toBe(OneHourInMilliseconds);
+    expect(readings[2].time - readings[3].time).toBe(OneHourInMilliseconds);
   });
 });
